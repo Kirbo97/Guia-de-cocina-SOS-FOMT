6a13888dbfb82b86aeb30eaf835151deb20d5855
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2024-04-09 14:25:32 -0500

    Add files via upload

--- JS/funciones.js
+++ JS/funciones.js
@@ -3,9 +3,21 @@ function copiar(){
     localStorage.setItem('anterior',urlActual); //guarda el valor 
 }
 
-function usar(){
+
+function usar(perfil){
     var accederAnterior = ''; //inicializa la variable que se usara para obtener el dato
     accederAnterior = localStorage.getItem('anterior'); //acceder al valor 
     let anterior = document.getElementById('atras'); //La imagen cambia
-    anterior.setAttribute("href",accederAnterior);
+
+    alert(accederAnterior);
+
+    if(accederAnterior == window.location.href){
+
+        if(perfil == "can"){ anterior.setAttribute("href","../../Otros/Casamiento.html"); } else
+
+        if(perfil == "ald"){ anterior.setAttribute("href","../../Aldeanos.html"); }
+
+    } else { 
+        anterior.setAttribute("href",accederAnterior); 
+    }
 }
