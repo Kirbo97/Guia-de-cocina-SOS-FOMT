b60beefda71804a8aeed6e7d3941285ad3b96de3
Fabian Daquilema <109368214+Kirbo97@users.noreply.github.com>
2024-04-10 19:39:03 -0500

    Add files via upload

--- JS/funciones.js
+++ JS/funciones.js
@@ -10,5 +10,4 @@ function usar(perfil){
     let ant = document.getElementById('a1'); //Creo una coneccion a la etiqueta con la ID
         if(perfil == "can"){ ant.setAttribute("href","../../Otros/Casamiento.html"); }
         if(perfil == "ald") { ant.setAttribute("href","../../Aldeanos.html"); }
-
 }
